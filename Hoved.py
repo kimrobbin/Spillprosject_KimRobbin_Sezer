eb573bae7780fbcffc150e7041d732e0925d4625
kimrobbin <156893524+kimrobbin@users.noreply.github.com>
2025-11-17 15:08:57 +0100

    Added the pokemon api connection part

--- Hoved.py
+++ Hoved.py
@@ -1,15 +1,19 @@
+import requests
+
+web = "https://pokeapi.co/api/v2/"
+
+def pokemon_get(name):
+    
+    url= f"{web}pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        return response.json()
+    else:
+        print("Noe gikk galt")
+        input("Trykk noe for å fortsette...")
 
 
 
 
-
-
-
-def main():
-    while True:
-        pass
-
-    pass
-
-
-main()
