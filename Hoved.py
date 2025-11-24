86ba5d83f095cab98cddbea23652728142fc3051
kimrobbin <156893524+kimrobbin@users.noreply.github.com>
2025-11-24 15:48:20 +0100

    flyttet class i egen fil

--- Hoved.py
+++ Hoved.py
@@ -1,11 +1,9 @@
 import requests
 import random
-import os  
+import os 
+from pokemon import Pokemon
 
-class Pokemon:
-    def __init__(self, name, weight):
-        self.name = name
-        self.weight = weight
+
 
 # Makes a link to the api
 web = "https://pokeapi.co/api/v2/"
@@ -29,7 +27,10 @@ def linje():
 def main():
     ny_pokemon_get = random.randint(1, 1025)
     ny_pokemon_get = get_pokemon(ny_pokemon_get)
+
+    # Makes a object for gammel pokemon
     gammel_pokemon = Pokemon(ny_pokemon_get['name'].title(), ny_pokemon_get['weight'] / 10)
+
     score = 0
 
 
@@ -42,47 +43,50 @@ def main():
 
         
         linje()
-        
-        ny_pokemon_class = Pokemon(ny_pokemon_get['name'].title(), ny_pokemon_get['weight'] / 10)
-        print(f"Veier {gammel_pokemon.name} ( {gammel_pokemon.weight} kg) mer en {ny_pokemon_class.name}? ")
-        print(f"Navn: {ny_pokemon_class.name}")
+         # Makes a object for ny pokemon
+        ny_pokemon = Pokemon(ny_pokemon_get['name'].title(), ny_pokemon_get['weight'] / 10)
+        print(f"Veier {gammel_pokemon.name} ( {gammel_pokemon.weight} kg) mer en {ny_pokemon.name}? ")
+        print(f"Navn: {ny_pokemon.name}")
         print(f"Score: {score}")
 
 
         linje()
         while True:
             player_input = input("Y or N: ").lower()
+
             if player_input == "y":
-                if gammel_pokemon.weight > ny_pokemon_class.weight:
+                # If gammel pokemon weights more then ny pokemon you get point 
+                if gammel_pokemon.weight > ny_pokemon.weight:
                     print("Det var riktig ")
-                    print(f"Den veier {ny_pokemon_class.weight}")
+                    print(f"Den veier {ny_pokemon.weight}")
                     score += 1
                     input("Trykk en knapp... ")
                 else:
                     print("Det var feil ")
-                    print(f"Den veier {ny_pokemon_class.weight}")
+                    print(f"Den veier {ny_pokemon.weight}")
                     print(f"Scoren din ble: {score}")
                     input("Trykk en knapp å avslutte... ")
                     quit()
                 break
 
             elif player_input == "n":
-                if gammel_pokemon.weight <= ny_pokemon_class.weight:
+                if gammel_pokemon.weight <= ny_pokemon.weight:
                     print("Det var riktig ")
-                    print(f"Den veier {ny_pokemon_class.weight}")
+                    print(f"Den veier {ny_pokemon.weight}")
                     score += 1
                     input("Trykk en knapp... ")
                 else:
                     print("Det var feil ")
-                    print(f"Den veier {ny_pokemon_class.weight}")
+                    print(f"Den veier {ny_pokemon.weight}")
                     print(f"Scoren din ble: {score}")
                     input("Trykk en knapp for å avslutte... ")
                     quit()
                 break
             else:
                 print("skriv inn Y eller N")
+
         # clears the terminal
         os.system("cls")
-        gammel_pokemon = ny_pokemon_class
+        gammel_pokemon = ny_pokemon
 
 main()
